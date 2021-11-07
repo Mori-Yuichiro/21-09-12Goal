c529811cd33d2462eb8d2ac2784d4f891042c60d
森祐一朗 <apotikisin4632@gmail.com>
2021-11-07 21:28:26 +0900

    fix code

--- src/components/home/Home.ts
+++ src/components/home/Home.ts
@@ -9,26 +9,21 @@ import Modal from '../modal/Modal.vue';
 
 export default class Home extends Vue {
     private todo = '';
-    private todoList:string[] = [];
+    private todoList: string[] = [];
     private modal = false;
 
     private addTodo()  {
-        console.log(this.todo);
         this.todoList.push(this.todo);
-        console.log(this.todoList);
         this.todo = '';
         this.modal = false;
     }
 
     private remove(index: number) {
-        console.log('remove');
         this.todoList.splice(index, 1);
     }
-    
     // Modal
     private openDialog() {
         this.modal = true;
-        console.log(this.modal);
     }
 
     private closeDialog() {
